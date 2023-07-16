4013ef8664b21b3bcb491c0f2d9401dbe4be3d2f
TheKobag <gabespe83@gmail.com>
2023-07-16 16:23:14 +0200

    chore() added ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
@@ -11,8 +13,10 @@ import { GraphQLModule } from './graphql.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule, 
+    HttpClientModule,
     GraphQLModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot({}),
   ],
   providers: [],
   bootstrap: [AppComponent],
