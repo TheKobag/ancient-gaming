7290cf377fb03969d0ce6502b9336d4610f02154
TheKobag <gabespe83@gmail.com>
2023-07-16 18:07:10 +0200

    chore(): added ng-mocks

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,20 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { DefaultRenderComponent, MockBuilder, MockedComponentFixture, MockRender } from 'ng-mocks';
 import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
+
 
 describe('AppComponent', () => {
+  let fixture: MockedComponentFixture<AppComponent>;
+  let component: DefaultRenderComponent<AppComponent>;
+
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+    await MockBuilder(AppComponent, AppModule);
+
+    fixture = MockRender(AppComponent);
+    component = fixture.point.componentInstance;
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'ancient-gaming'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ancient-gaming');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ancient-gaming app is running!');
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 });
