0f7ad5e7d0740c5147b38648ed23a5d468e6a440
TheKobag <gabespe83@gmail.com>
2023-07-17 02:40:58 +0200

    feat(add): added form

---
+++ src/app/posts/ui/components/post-form/post-form.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-post-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './post-form.component.html',
+  styleUrls: ['./post-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PostFormComponent {
+  @Input() postForm!: FormGroup;
+  @Output() submitForm = new EventEmitter<any>();
+}
--- src/app/posts/ui/containers/add/add.component.ts
+++ src/app/posts/ui/containers/add/add.component.ts
@@ -1,14 +1,36 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PostFormComponent } from '../../components/post-form/post-form.component';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PostFormComponent],
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AddComponent {
+export class AddComponent implements OnInit {
+  postForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
+  ngOnInit(): void {
+    this.postForm = this.fb.group({
+      title: new FormControl('', Validators.required),
+      body: new FormControl('', Validators.required),
+    });
+  }
+
+  onSubmitForm(submit: boolean): void {
+    if (submit) {
+      console.log(this.postForm);
+      // update store
+    }
+  }
 }
